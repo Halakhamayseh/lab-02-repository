33711146bc38adcd75de4851e1fd097ba8155b8b
Halakhamayseh <khamaysehhala95@yahoo.com>
2021-04-22 04:33:28 +0300

    lab 03 not completed yet

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 'use stict';
-const opKeyArray = [];
+const opKeyArray1 = [];
+const opKeyArray2 = [];
 const objArr = [];
 function An(nObject) {
   this.title = nObject.title;
@@ -12,80 +13,133 @@ function An(nObject) {
 
 
 An.prototype.render = function () {
-  let nObjectClone = $('.nObjectTemplate').clone();
-  $('main').append(nObjectClone);
-  nObjectClone.find('h3').text(this.title);
-  nObjectClone.find('img').attr('src', this.image_url);
-  nObjectClone.find('img').attr('alt', `${this.keyword}`);
-  nObjectClone.find('p').text(this.description);
-    nObjectClone.attr("class",`${this.keyword}`);
-    
-    
+  let templating = $('#templating').html();
+  let templatingMergeObj = Mustache.render(templating, this);
+  $('main').append(templatingMergeObj);
+  // let nObjectClone  = $('.nObjectTemplate').clone();
+  // nObjectClone.attr("class", `${this.keyword}`);
 };
-// console.log(opKeyArray);
-function readJson() {
+///page1handel/////////////////////////////////////
+function readJsonPage1() {
   const ajaxSet = {
     method: 'get',
     dataType: 'json'
   };
   $.ajax('data/page-1.json', ajaxSet).then(make);
 }
+
 function make(data) {
   data.forEach((item) => {
     let nnObject = new An(item);
-    //   console.log(nnObject.keyword);
-      nnObject.render();
-      
-    if (opKeyArray.includes(nnObject.keyword) === false) {
-      opKeyArray.push(nnObject.keyword);
+    nnObject.render();
+    if (opKeyArray1.includes(nnObject.keyword) === false) {
+      opKeyArray1.push(nnObject.keyword);
 
     }
 
   });
-  keyRender();
+  keyRender1();
+}
+readJsonPage1();
+
+$('#pageOne').on('click', clickHandel);
+function clickHandel() {
+  let pageOne = $(this).val();
+  console.log(pageOne);
+
+  if (pageOne === 'page1Click') {
+    $('div').hide();
+    readJsonPage1();
+    filtirH();
+    $('div').hide();
+  }
 }
 
-readJson();
+////////////////////////////////////////////////////
+//////page 2 handel///
+function readJsonPage2() {
+  const ajaxSet = {
+    method: 'get',
+    dataType: 'json'
+  };
+  $.ajax('data/page-2.json', ajaxSet).then(makeTwo);
+}
+function makeTwo(data) {
+  data.forEach((item) => {
+    let nnObject = new An(item);
+    nnObject.render();
+    if (opKeyArray2.includes(nnObject.keyword) === false) {
+      opKeyArray2.push(nnObject.keyword);
+
+    }
+
+  });
+  keyRender2();
+}
+
+$('#pageTwo').on('click', clickHandeTwo);
+function clickHandeTwo() {
+  let pageTwo = $(this).val();
+  console.log(pageTwo);
+  if (pageTwo === 'page2Click') {
+    $('div').hide();
+    readJsonPage2();
+    filtirH();
+  }
+}
+
+////////////////////////////////////////////////////////////////
 ///////filiter/////
 
-function keyRender() {
-  opKeyArray.forEach(e => {
-      $('select').append(`<option value=${e}>${e}</option>`);
+function keyRender1() {
+  opKeyArray1.forEach(e => {
+    $('select').append(`<option value=${e}>${e}</option>`);
   });
-}
-$('select').on('change', changeHandel);
-function changeHandel() {
-  let ss = $(this).val();
-  if (ss !== 'default') {
-    $('div').hide();
-    $(`.${ss}`).show();
-    ////other way to slove this issue///
-    // $('div').toArray().forEach((e) => {
-    //   console.log(e.classList);
-    //   if (e.classList.contains(ss)) {
-    //     $(`.${ss}`).show();
-    //   }
-    // });
-   }
-    //   objArr.forEach(element => {
-    //       if (element===ss) {
-            
-    //       }
-    //     });
-    //     console.log(ss);
-    // }
-    // event.preventDefault();
-    // console.log(event);
-    // for (let i = 0; i < opKeyArray.length; i++) {
-    //     // console.log(opKeyArray[0]);
-    //     console.log(event);
-    //     if (event === opKeyArray[i]) {
-    //         console.log('g');
-    //         $('div').hide();
-    //         $(`.${sel}`).show();
-    //     }
-        
-    // }
-   
-}
 
+}
+function keyRender2() {
+  opKeyArray2.forEach(e => {
+    $('select').append(`<option value=${e}>${e}</option>`);
+  });
+
+}
+function filtirH() {
+  $('select').on('change', changeHandel);
+  function changeHandel() {
+    let ss = $(this).val();
+    if (ss !== 'default') {
+      $('div').hide();
+      $(`.${ss}`).show();
+    }
+  }
+}
+filtirH();
+
+////other way to slove this issue///
+//   $('div').toArray().forEach((e) => {
+//     console.log(e.classList);
+//     if (e.classList.contains(ss)) {
+//       $(`.${ss}`).show();
+//     }
+//   });
+//  }
+//   objArr.forEach(element => {
+//       if (element===ss) {
+//       }
+//     });
+//     console.log(ss);
+// }
+// event.preventDefault();
+// console.log(event);
+// for (let i = 0; i < opKeyArray.length; i++) {
+//     // console.log(opKeyArray[0]);
+//     console.log(event);
+//     if (event === opKeyArray[i]) {
+//         console.log('g');
+//         $('div').hide();
+//         $(`.${sel}`).show();
+//     }
+
+// }
+
+// }
